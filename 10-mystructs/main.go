43fd28f8425c757fba08c2101317c5f31e0fb80a
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-02-07 07:43:29 +0530

    Completed 10

--- 10-mystructs/main.go
+++ 10-mystructs/main.go
@@ -7,7 +7,7 @@ func main() {
 
 	pvarma := User{"Pradeep Varma", "spvpenumatsa2@gmail.com", 20, 123456789, true}
 	fmt.Println("New user Log : ", pvarma)
-	fmt.Printf("Formatted New User's Details : \n%+v\n", pvarma)
+	fmt.Printf("Formatted New User's Details : \n%+v\n", pvarma) //placeholder to format details
 	fmt.Printf("New User's Name : %v & New User's Email: %v\n", pvarma.Name, pvarma.Email)
 }
 
