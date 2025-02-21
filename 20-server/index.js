bb95ff6966d8324d8e35c8c5a7e0d89f31e3f611
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-02-21 08:48:31 +0530

    Added Server for GO

---
+++ 20-server/index.js
@@ -0,0 +1,33 @@
+// This is server created for GO
+// we will use this server to fetch Data using GO
+// install node and try to run this server by command "npm start"
+// make sure to run this server and proceed to next folders
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.send("Hello, Welcome to GO Lang Demo Server")
+})
+
+app.get("/get",(req,res)=>{
+    res.json({"message":"Hello, I am Pradeep Varma"})
+})
+
+app.post("/post",(req,res)=>{
+    let body = req.body
+    res.send(body)
+})
+
+app.post("/postform",(req,res)=>{
+    res.send(JSON.stringify(req.body))
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is Running on  http://localhost:${port}`);
+    
+})
