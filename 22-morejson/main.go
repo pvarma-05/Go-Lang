c3b6198a99abe32f83a885fe5bfcc545799b1dc1
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-02-23 11:55:06 +0530

    Added JSON Decoding

--- 22-morejson/main.go
+++ 22-morejson/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to JSON Handling")
-	encodeJson()
+	encodeJson() //this func is for encoding JSON
+	decodeJson() //this func is for decoding JSON
 }
 
 func encodeJson() {
@@ -32,4 +33,44 @@ func encodeJson() {
 	}
 
 	fmt.Printf("%s\n", res)
+}
+
+func decodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+                "coursename": "React JS",
+                "cost": 20,
+                "website": "Youtube",
+                "tags": [
+                        "Frontend",
+                        "JS"
+                ]
+    }
+	`)
+
+	var mycourse course
+
+	//1st way
+
+	isValid := json.Valid(jsonDataFromWeb)
+	if isValid {
+		fmt.Println("JSON Data is Valid")
+		json.Unmarshal(jsonDataFromWeb, &mycourse)
+		// fmt.Printf("%#v\n", mycourse)
+	} else {
+		fmt.Println("JSON IS NOT VALID")
+	}
+
+	// some cases where we want to add key to data
+	// 2nd way
+
+	var myData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myData)
+	// fmt.Printf("%#v\n", myData)
+
+	//more formatted Version
+	for k, v := range myData {
+		fmt.Printf("Key is %v and Value is %v and Type is %T\n", k, v, v)
+	}
+
 }
